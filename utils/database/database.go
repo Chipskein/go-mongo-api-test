7bd438c5e409ed74344d952341757cdcdf6d97fe
Chipskein <abfn0905@gmail.com>
2022-08-13 12:50:00 -0300

    refactoring

--- utils/database/database.go
+++ utils/database/database.go
@@ -11,15 +11,16 @@ import (
 
 var Client *mongo.Client
 
+func GetMongoClient() *mongo.Client {
+	return Client
+}
+
 func CreateMongoClient(mongodb_url string) {
 	log.Print("mongodb_url => ", mongodb_url)
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), 20*time.Second)
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongodb_url))
 	if err != nil {
 		log.Panic(err)
 	}
 	Client = client
-}
-func GetMongoClient() *mongo.Client {
-	return Client
 }
